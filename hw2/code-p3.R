54b98d1fecadf8bc8c71623c06d8d5abf950d024
Sam Laane <samlaane@gmail.com>
2014-03-04 22:54:15 -0600

    bugfix on part 3

--- hw2/code-p3.R
+++ hw2/code-p3.R
@@ -56,71 +56,74 @@ corpus <- tm_map(corpus, stripWhitespace)
 # Build a document term matrix from the corpus.
 dtm <- DocumentTermMatrix(corpus)
 
-termCount = c(".94", ".96", ".98")
+termCount = c(".94", ".95",".97",".98",".99")
 termCount
-
+spam <- new.env()
+accuracy<- new.env()
+kaccuracy <- new.env()
 
 for (v in termCount){
     # Remove sparse terms to get a managable number of terms.
     dtmtmp <- removeSparseTerms(dtm, as.numeric(v))
-    as.numeric(v)
     # Convert the document term matrix to a standard matrix.
     freqMatrix <- as.data.frame( as.matrix(dtmtmp) )
     # Normalize the frequency matrix: 0 if absent, 1 if present.
-    spam <- (freqMatrix > 0) + 0 # Add 0 to convert from logical to int.
+    spamt <- (freqMatrix > 0) + 0 # Add 0 to convert from logical to int.
     # Delete the header names to avoid collisions with R reserved words.
-    spam <- matrix(spam, ncol=ncol(spam), dimnames=NULL)
+    spam[[v]] <- matrix(spamt, ncol=ncol(spamt), dimnames=NULL)
+    accuracy[[v]] <- 0
+    kaccuracy[[v]] <- 0
 }
 
-
 # 10 fold cross validation (data divided in 10 parts randomly sampled)
-accuracy <- 0
-kaccuracy <- 0
 accuracyOfAssumingHam <- 0
 folds <- cvFolds(nrow(spam), K=10)
 for(i in 1:10) {
-  # Print iteration.
-  printf("Iterarion %d", i)
+    # Print iteration.
+    printf("Iterarion %d", i)
+    for (v in termCount){
+        # Separate train and test data and labels.
+        trainData <- spam[[v]][folds$subsets[folds$which != i], ]
+        testData  <- spam[[v]][folds$subsets[folds$which == i], ]
+        trainLabels <- trueLabels[folds$subsets[folds$which != i]]
+        testLabels  <- trueLabels[folds$subsets[folds$which == i]]
 
-  # Separate train and test data and labels.
-  trainData <- spam[folds$subsets[folds$which != i], ]
-  testData  <- spam[folds$subsets[folds$which == i], ]
-  trainLabels <- trueLabels[folds$subsets[folds$which != i]]
-  testLabels  <- trueLabels[folds$subsets[folds$which == i]]
+        # Random Forest classifier:
+        #  - Create a random forest using the training data.
+        clf <- randomForest(factor(trainLabels) ~ ., data=trainData, ntree=30)
 
-  # Random Forest classifier:
-  #  - Create a random forest using the training data.
-  clf <- randomForest(factor(trainLabels) ~ ., data=trainData, ntree=30)
-  
-  #  - Classify the test data with the random forest classifier.
-  labels <- predict(clf, testData)
-  
-  #  - Get the number of correctly classified samples.
-  hits <- length(labels[labels == testLabels])
-  accuracy <- accuracy + (hits / nrow(testData))
-  
-  # Nearest Neighbours classifier:
-  #  - Classify using fields 1 to 54 as it gets far better results than 1 to 57.
-  klabels<- knn(trainData, testData, trainLabels, k = 1, prob = FALSE,
-              algorithm=c("kd_tree"))
-              
-  #  - Get the number of correctly classified samples.
-  khits <- length(klabels[klabels == testLabels])
-  kaccuracy <- kaccuracy + (khits /nrow(testData))
-
-  ahits <- nrow(testData) -sum(testLabels)
-  accuracyOfAssumingHam <- accuracyOfAssumingHam + (ahits/nrow(testData))
-  
+        #  - Classify the test data with the random forest classifier.
+        labels <- predict(clf, testData)
+        
+        #  - Get the number of correctly classified samples.
+        hits <- length(labels[labels == testLabels])
+        accuracy[[v]] <- accuracy[[v]] + (hits / nrow(testData))
+        
+        # Nearest Neighbours classifier:
+        #  - Classify using fields 1 to 54 as it gets far better results than 1 to 57.
+        klabels<- knn(trainData, testData, trainLabels, k = 1, prob = FALSE,
+                      algorithm=c("kd_tree"))
+        
+        #  - Get the number of correctly classified samples.
+        khits <- length(klabels[klabels == testLabels])
+        kaccuracy[[v]] <- kaccuracy[[v]] + (khits /nrow(testData))
+    }
+        ahits <- nrow(testData) -sum(testLabels)
+        accuracyOfAssumingHam <- accuracyOfAssumingHam + (ahits/nrow(testData))
+    
 }
 
-# Print the resulting accuracies.
-accuracy <- (accuracy / 10) * 100
-kaccuracy <- (kaccuracy / 10) * 100
 accuracyOfAssumingHam <- (accuracyOfAssumingHam/ 10) * 100
 
-printf("Random forest accuracy: %.2f %%", accuracy)
-printf("KNN accuracy: %.2f %%", kaccuracy)
-printf("Assuming nothing is spam %.2f %%", accuracyOfAssumingHam)
-
-ncol(spam)
-nrow(spam)
+for (v in termCount){
+    # Print the resulting accuracies.
+    accuracy[[v]] <- (accuracy[[v]] / 10) * 100
+    kaccuracy[[v]] <- (kaccuracy[[v]] / 10) * 100
+   
+    
+    printf("Random forest accuracy: %.2f %%", accuracy[[v]])
+    printf("KNN accuracy: %.2f %%", kaccuracy[[v]])
+    printf("Assuming nothing is spam %.2f %%", accuracyOfAssumingHam)
+    print(ncol(spam[[v]]))
+    print(nrow(spam[[v]]))
+}
