acb7b8924f8cfc0abdc8c7f14660cfa86ff40354
José Vicente Ruiz <josruice@upv.es>
2014-04-09 01:37:04 -0500

    Implementing brute force nearest neighbour question marks adapted.

--- hw3/crimes.R
+++ hw3/crimes.R
@@ -92,6 +92,8 @@ for(i in attr_with_unkw){
     unk_indices <- which(clean_data[,i] == '?')
     i_unk_data <- no_unkw_data[unk_indices, ]
     i_no_unk_data <- no_unkw_data[-unk_indices, ]
+
+
     i_klabels <- knn(i_no_unk_data,
                    i_unk_data,
                    i_no_unk_data$ViolentCrimesPerPop, k = 1,
@@ -101,7 +103,60 @@ for(i in attr_with_unkw){
     
 }
 
+nn_data <- clean_data
+for(i in 1:nrow(clean_data)){
+  min_distance <- -1
+  min_index <- -1
+  for(j in 1:nrow(clean_data)){
+    if(i != j){
+      d <- distance(clean_data[i,], clean_data[j,])
+      if(min_distance == -1 | d < min_distance){
+        min_distance <- d
+        min_index <- j
+      }
+    }
+  }
+  nn_data[i,] <- clean_data[min_index,]
+  print(i)
+}
 
+# Compute the euclidean distance between two vectors that may contain question 
+# mark elements.
+distance <- function(v1, v2){
+  # Change question mark elements of each vector by the same position elements
+  # in the other vector, so that they get cancelled when computing the distance.
+  unk_indices_1 <- which(v1 == '?')
+  unk_indices_2 <- which(v2 == '?')
+
+  v1[unk_indices_1] = v2[unk_indices_1]
+  v2[unk_indices_2] = v1[unk_indices_2]
+
+  # Change question marks elements with same position in both vectors by 0.
+  unk_indices <- which(v1 == '?')
+  v1[unk_indices] <- 0
+  v2[unk_indices] <- 0
+
+  # Ensure vectors type is numeric.
+  v1 <- as.numeric(v1)
+  v2 <- as.numeric(v2)
+
+  # Return the euclidian distance.
+  return(sqrt(sum((v1 - v2) ^ 2)))
+}
+
+
+distance <- function(v1, v2){
+  s <- 0
+  for(i in 1:length(v1)){
+    if(v1[i] != '?' & v2[i] != '?'){
+      s <- s + ((as.numeric(v1[i]) - as.numeric(v2[i])) ^ 2)
+    }
+  }
+  return(sqrt(s))
+}
+
+
+outer(clean_data, clean_data, FUN = distance)
 
 # EXTRA:
 # Data visualization commands:
